4297b9bd1417a09b53cea7add8d4be8328b985e1
siddharth kadu <103137013+siddharthkadu@users.noreply.github.com>
2025-04-03 15:12:04 +0530

    Update add.java

--- add.java
+++ add.java
@@ -9,3 +9,21 @@ class Main{
 
 	}
 }
+
+
+
+//multiplication code 
+
+public class MultiplyTwoNumbers {
+    public static void main(String[] args) {
+        // Hardcoded values
+        int num1 = 10;
+        int num2 = 20;
+
+        // Multiplication
+        int product = num1 * num2;
+
+        // Output result
+        System.out.println("Product: " + product);
+    }
+}
