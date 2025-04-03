92915af3caffddd1d6ee6c879005bc4d539748b0
Sid-0405 <siddharthkadu2001@gmail.com>
2025-04-03 15:13:35 +0530

    sub code added

--- add.java
+++ add.java
@@ -8,4 +8,27 @@ class Main{
 	 System.out.println(ans);
 
 	}
+}   
+
+
+
+
+
+
+
+
+
+public class SubtractTwoNumbers {
+    public static void main(String[] args) {
+        // Hardcoded values
+        int num1 = 20;
+        int num2 = 10;
+
+        // Subtraction
+        int difference = num1 - num2;
+
+        // Output result
+        System.out.println("Difference: " + difference);
+    }
 }
+
